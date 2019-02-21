3fa122c8d818413d8f3f1c023479c2a0ec510a93
delphinus <delphinus@remora.cx>
2019-02-21 15:45:59 +0900

    Return candidates only

--- rplugin/python3/deoplete/sources/firestore.py
+++ rplugin/python3/deoplete/sources/firestore.py
@@ -42,54 +42,38 @@ class Source(Base):
         return m.start() if m else -1
 
     def gather_candidates(self, context: Any) -> CANDIDATES:
-        (candidates, input_str) = self._search_candidates(context)
-        return (
-            candidates
-            if input_str == ""
-            else [x for x in candidates if x["word"].startswith(input_str)]
-        )
-
-    def _search_candidates(self, context: Any) -> Tuple[CANDIDATES, str]:
         allow_match = re.search(r"allow\s+([a-z\s,]*)$", context["input"])
         if allow_match:
-            (candidates, input_str) = self._access_control_candidates(
-                context, allow_match.group(1)
-            )
+            candidates = self._access_control_candidates(context, allow_match.group(1))
             debug(self.vim, ("f4", candidates))
             if candidates:
-                return (candidates, input_str)
+                return candidates
         method_match = re.search(r".+\.\w*$", context["input"])
         if method_match:
-            (candidates, input_str) = self._method_candidates(
-                context, method_match.group(0)
-            )
+            candidates = self._method_candidates(context, method_match.group(0))
             if candidates:
-                return (candidates, input_str)
+                return candidates
         global_match = re.search(r"[^.]\b([a-zA-Z]*)$", context["input"])
         if global_match:
-            (candidates, input_str) = self._top_candidates(
-                context, global_match.group(1)
-            )
-            if candidates:
-                return (candidates, input_str)
-        return ([], "")
+            return self.__globals
+        return []
 
-    def _top_candidates(self, context: Any, matched: str) -> Tuple[CANDIDATES, str]:
-        return (self.__globals, matched)
+    def _top_candidates(self, context: Any, matched: str) -> CANDIDATES:
+        return self.__globals
 
-    def _method_candidates(self, context: Any, matched: str) -> Tuple[CANDIDATES, str]:
-        (var, input_str) = matched.rsplit(".", 1)
+    def _method_candidates(self, context: Any, matched: str) -> CANDIDATES:
+        (var,) = matched.rsplit(".", 1)
         properties = self.__methods.get(var, None)
         if properties:
-            return (properties, input_str)
+            return properties
         if var.endswith("]"):
             methods = self.__types.get("list", [])
-            return (methods, input_str)
+            return methods
         if var.endswith(")"):
-            return (self._func_return_value_methods(context, var), input_str)
+            return self._func_return_value_methods(context, var)
         if "." in var:
-            return (self._parent_methods(context, var), input_str)
-        return ([], "")
+            return self._parent_methods(context, var)
+        return []
 
     def _func_return_value_methods(self, context: Any, var: str) -> CANDIDATES:
         if len(var) < 2:
@@ -130,12 +114,10 @@ class Source(Base):
                 return self.__types[prop_type]
         return []
 
-    def _access_control_candidates(
-        self, context: Any, matched: str
-    ) -> Tuple[CANDIDATES, str]:
+    def _access_control_candidates(self, context: Any, matched: str) -> CANDIDATES:
         debug(self.vim, ("f2", matched))
         if matched == "":
-            return (self.__access_controls, "")
+            return self.__access_controls
         words = re.sub(r"\s+", "", matched).split(",")
         last_word = words[-1]
         word_set = set(words)
@@ -152,17 +134,4 @@ class Source(Base):
 
         selected = reduce(gather, self.__access_controls, cast(Set[str], set()))
         debug(self.vim, ("f3", last_word, selected))
-        if last_word == "":
-            return (
-                [x for x in self.__access_controls if x["word"] not in selected],
-                last_word,
-            )
-        return (
-            [
-                x
-                for x in self.__access_controls
-                if x["word"] == last_word
-                or (x["word"].startswith(last_word) and x["word"] not in selected)
-            ],
-            last_word,
-        )
+        return [x for x in self.__access_controls if x["word"] not in selected]
