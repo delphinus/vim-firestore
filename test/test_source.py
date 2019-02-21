8ba33209375388e719f5631c0ff5c3346dedabfc
delphinus <delphinus@remora.cx>
2019-02-21 17:14:24 +0900

    Add tests for access controls

--- test/test_source.py
+++ test/test_source.py
@@ -1,13 +1,30 @@
 import pynvim
-from os.path import dirname, isfile, join
-from os import unlink
-import sys
-
+import pytest
 from sources.firestore import Source
+from typing import List, Set
 
 
-def test_hoge(vim: pynvim.Nvim) -> None:
-    vim.command("normal itesting\npython\napi")
-    vim.command("w! /tmp/test.txt")
-    assert isfile("/tmp/test.txt")
-    unlink("/tmp/test.txt")
+@pytest.fixture
+def source(vim: pynvim.Nvim) -> Source:
+    source = Source(vim)
+    source.on_init({})
+    return source
+
+
+@pytest.mark.parametrize(
+    "input_str,expected",
+    [
+        ("allow", {}),
+        ("allow ", {"read", "write", "get", "list", "create", "update"}),
+        ("allow r", {"read", "write", "get", "list", "create", "update"}),
+        ("allow read", set([])),
+        ("allow read,", {"write", "create", "update"}),
+        ("allow read,c", {"write", "create", "update"}),
+        ("allow read,create", set([])),
+        ("allow read,create,", {"update"}),
+        ("allow read,create,update", set([])),
+    ],
+)
+def test_candidates(source: Source, input_str: str, expected: Set[str]) -> None:
+    candidates = source.gather_candidates({"input": input_str})
+    assert [x["word"] for x in candidates], expected
